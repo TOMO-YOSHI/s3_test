512b12412c0d3c8c43ff93073e424e4b5cc9c397
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-02-01 00:07:46 -0800

    first commit

---
+++ react-app-s3/src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      success : false,
+      url : ""
+    }
+  }
+  
+  handleChange = (ev) => {
+    this.setState({success: false, url : ""});
+    
+  }
+
+  // Perform the upload
+  handleUpload = (ev) => {
+    let file = this.uploadInput.files[0];
+    // Split the filename to get the name and type
+    let fileParts = this.uploadInput.files[0].name.split('.');
+    let fileName = fileParts[0];
+    let fileType = fileParts[1];
+    console.log("Preparing the upload");
+    fetch("http://localhost:8000/sign_s3",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({fileName: fileName, fileType: fileType}),
+      }
+    ).then(response => {
+      return response.json();
+    }).then( response => {
+      var returnData = response.data.returnData;
+      var signedRequest = returnData.signedRequest;
+      var url = returnData.url;
+      this.setState({url: url})
+      console.log("Recieved a signed request " + signedRequest);
+      
+     // Put the fileType in the headers for the upload
+      // var options = {
+      //   headers: {
+      //     'Content-Type': fileType
+      //   }
+      // };
+      if (fileType === "mp4" || fileType === "mov") {
+        fileType = "video/" + fileType;
+      }    
+      fetch(signedRequest, {
+        method: "PUT",
+        headers: { "Content-Type": fileType },
+        body: file
+        })
+      // fetch(signedRequest,file,options)
+      .then(result => {
+        console.log("Response from s3")
+        this.setState({success: true});
+      })
+      .catch(error => {
+        alert("ERROR " + JSON.stringify(error));
+      })
+    })
+    .catch(error => {
+      alert(JSON.stringify(error));
+    })
+  }
+
+  render() {
+    const Success_message = () => (
+      <div style={{padding:50}}>
+        <h3 style={{color: 'green'}}>SUCCESSFUL UPLOAD</h3>
+        <a href={this.state.url}>Access the file here</a>
+        <br/>
+      </div>
+    )
+    return (
+      <div className="App">
+        <center>
+          <h1>UPLOAD A FILE</h1>
+          {this.state.success ? <Success_message/> : null}
+          <input onChange={this.handleChange} ref={(ref) => { this.uploadInput = ref; }} type="file"/>
+          <br/>
+          <button onClick={this.handleUpload}>UPLOAD</button>
+        </center>
+      </div>
+    );
+  }
+}
+export default App;
